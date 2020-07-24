6927e65359d641b6d61edfdf593f017ba31177cc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-24 14:04:29 +0000

    Deploy 91-zhangshengtao/iantd-react to github.com/91-zhangshengtao/iantd-react.git:gh-pages

---
+++ precache-manifest.0fa2ada6fbf3ff524308b3093e23483e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c034b21e0d1eb2942634787669df38e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.16ec0f7e4ed7fde2c955.bundle.js"
+  },
+  {
+    "url": "runtime~main.16ec0f7e4ed7fde2c955.bundle.js"
+  },
+  {
+    "revision": "dee60d0f0033edd9c53d",
+    "url": "static/css/main.5d30f181.chunk.css"
+  },
+  {
+    "revision": "1fe3163d774f8043ee6a",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.16ec0f7e4ed7fde2c955.bundle.js"
+  },
+  {
+    "url": "vendors~main.16ec0f7e4ed7fde2c955.bundle.js.LICENSE.txt"
+  }
+]);
